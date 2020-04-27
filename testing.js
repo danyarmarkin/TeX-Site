7028976ec5c1824183d1c8cc6dca5980ea82c9ab
Danila Yarmarkin <danila.iarmarkin@gmail.com>
2020-04-27 12:46:19 +0300

    Добавил 2й вопрос в тесте и алгоритм, который обрабатывает TeX

--- testing.js
+++ testing.js
@@ -4,9 +4,17 @@
   var math = null;
               // the element jax for the math output.
   QUEUE.Push(function () {
-    math = MathJax.Hub.getAllJax("answer1")[0];
+    math1 = MathJax.Hub.getAllJax("answer1")[0];
+    math2 = MathJax.Hub.getAllJax("answer2")[0];
   });
-  window.UpdateMath = function (TeX) {
-    QUEUE.Push(["Text",math,"\\displaystyle{"+TeX+"}"]);
+  window.UpdateMath = function (num,TeX) {
+    if(num == 1){
+      QUEUE.Push(["Text",math1,"\\displaystyle{"+TeX+"}"]);
+    }else if(num == 2){
+      QUEUE.Push(["Text",math2,"\\displaystyle{"+TeX+"}"]);
+    }else if(num == 3){
+
+    }
+      
   }
 })();
