3a8f70d34df76a656d4e02099235ade6579a6e30
Danila Yarmarkin <danila.iarmarkin@gmail.com>
2020-04-27 13:22:35 +0300

    5 вопросов в тесте, но пока без функции создания нового вопроса

--- testing.js
+++ testing.js
@@ -6,6 +6,9 @@
   QUEUE.Push(function () {
     math1 = MathJax.Hub.getAllJax("answer1")[0];
     math2 = MathJax.Hub.getAllJax("answer2")[0];
+    math3 = MathJax.Hub.getAllJax("answer3")[0];
+    math4 = MathJax.Hub.getAllJax("answer4")[0];
+    math5 = MathJax.Hub.getAllJax("answer5")[0];
   });
   window.UpdateMath = function (num,TeX) {
     if(num == 1){
@@ -13,8 +16,11 @@
     }else if(num == 2){
       QUEUE.Push(["Text",math2,"\\displaystyle{"+TeX+"}"]);
     }else if(num == 3){
-
+      QUEUE.Push(["Text",math3,"\\displaystyle{"+TeX+"}"]);
+    }else if(num == 4){
+      QUEUE.Push(["Text",math4,"\\displaystyle{"+TeX+"}"]);
+    }else if(num == 5){
+      QUEUE.Push(["Text",math5,"\\displaystyle{"+TeX+"}"]);
     }
-      
   }
 })();
